1c515f51bd712d8f8196482b00f2fd3c5c15494d
Stepan Kolodkin <zorokon@yandex.ru>
2022-12-04 15:36:23 +0300

    initial

--- EuclidAlgorithm.py
+++ EuclidAlgorithm.py
@@ -6,3 +6,10 @@ def gcd_rem_division(num1, num2):
         else:
             num2 %= num1
     return num1 or num2
+
+
+def gcd(a, b):
+    """поиск НОД двух чисел"""
+    if a == b:
+        return a
+    return gcd(max(a, b) - min(a, b), min(a, b))
